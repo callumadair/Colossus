1a307219727e803a393bb97109664f3defd6d4b0
Cal <31387289+callumadair@users.noreply.github.com>
2020-10-28 13:59:53 +0000

    Update ShutDown.java

--- src/main/java/com/github/callumadair/ShutDown.java
+++ src/main/java/com/github/callumadair/ShutDown.java
@@ -1,12 +1,23 @@
 package com.github.callumadair;
 
+/**
+ * The type Shut down.
+ */
 class ShutDown extends BotAction {
 
+    /**
+     * Instantiates a new Shut down.
+     *
+     * @param bot the bot
+     */
     ShutDown(Bot bot) {
         super(bot);
     }
 
     @Override
+    /*
+     * Implements the shut down command for the bot.
+     */
     public void listener() {
         getBot().getApi().addMessageCreateListener(event -> {
             if (event.getMessageContent().equals(getBot().getPrefix() + "shutdown")
