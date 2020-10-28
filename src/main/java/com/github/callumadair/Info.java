b80ea5ed4153e50e3b11e5c203e46a7896ffb7ca
Cal <31387289+callumadair@users.noreply.github.com>
2020-10-28 13:57:29 +0000

    Update Info.java

--- src/main/java/com/github/callumadair/Info.java
+++ src/main/java/com/github/callumadair/Info.java
@@ -64,8 +64,9 @@ class Info extends BotAction {
             EmbedBuilder serverInfo = new EmbedBuilder();
             Server server = event.getServer().orElse(null);
 
-            assert server != null;
-            serverInfo.setTitle("Server Info").setColor(getBot().getRoleColour()).setDescription(server.getName())
+
+            serverInfo.setTitle("Server Info").setColor(getBot().getRoleColour())
+                    .setDescription(Objects.requireNonNull(server).getName())
                     .addField("Members", ":two_men_holding_hands: " + server.getMemberCount()
                             + " members (:robot: " + getBotCount(server) + " bots), " + getOnlineUsers(server)
                             + " online.")
