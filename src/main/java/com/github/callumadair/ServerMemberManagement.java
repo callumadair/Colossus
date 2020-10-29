faf798918cb8ba4ac216a69d04df81ab16de05c3
Cal <31387289+callumadair@users.noreply.github.com>
2020-10-29 10:42:23 +0000

    Create ServerMemberManagement.java

---
+++ src/main/java/com/github/callumadair/ServerMemberManagement.java
@@ -0,0 +1,18 @@
+package com.github.callumadair;
+
+public class ServerMemberManagement extends BotAction{
+
+    /**
+     * Instantiates a new BotAction.
+     *
+     * @param bot the specified bot
+     */
+    ServerMemberManagement(Bot bot) {
+        super(bot);
+    }
+
+    @Override
+    public void listener() {
+
+    }
+}
