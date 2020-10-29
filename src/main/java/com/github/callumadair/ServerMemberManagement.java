c4ffd59014684244638fb38ffa8fa715a2d0b961
Cal <31387289+callumadair@users.noreply.github.com>
2020-10-29 11:12:07 +0000

    Update ServerMemberManagement.java

--- src/main/java/com/github/callumadair/ServerMemberManagement.java
+++ src/main/java/com/github/callumadair/ServerMemberManagement.java
@@ -1,6 +1,11 @@
 package com.github.callumadair;
 
-public class ServerMemberManagement extends BotAction{
+import org.javacord.api.entity.user.*;
+import org.javacord.api.event.message.*;
+
+import java.util.*;
+
+public class ServerMemberManagement extends BotAction {
 
     /**
      * Instantiates a new ServerManagement object.
@@ -13,6 +18,35 @@ public class ServerMemberManagement extends BotAction{
 
     @Override
     public void listener() {
+        getBot().getApi().addMessageCreateListener(event -> {
+            kickSpecifiedMembers(event);
+            banSpecifiedMembers(event);
+        });
+    }
+
+    private void kickSpecifiedMembers(MessageCreateEvent event) {
+        if (event.getMessageAuthor().canKickUsersFromServer()) {
+            List<User> kickUsers = event.getMessage().getMentionedUsers();
+            StringBuilder kickMessage = new StringBuilder();
+            int count = 0;
+            for (User user : kickUsers) {
+                Objects.requireNonNull(event.getServer().orElse(null)).kickUser(user);
+                kickMessage.append(user.getMentionTag()).append(" ");
+                count++;
+            }
+
+            if (count == 1) {
+                kickMessage.append("was kicked!");
+            } else {
+                kickMessage.append("were kicked!");
+            }
+            event.getChannel().sendMessage(kickMessage.toString());
+        } else {
+            event.getChannel().sendMessage("You cannot kick users from the server.");
+        }
+    }
+
+    private void banSpecifiedMembers(MessageCreateEvent event) {
 
     }
 }
