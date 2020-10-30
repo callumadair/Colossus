267e52652efc28a96b59d620d949d54ba141cf33
Cal <31387289+callumadair@users.noreply.github.com>
2020-10-30 21:48:40 +0000

    reformatted

--- src/main/java/com/github/callumadair/Bot/BotAction.java
+++ src/main/java/com/github/callumadair/Bot/BotAction.java
@@ -1,38 +1,40 @@
 package com.github.callumadair.Bot;
 
-import com.github.callumadair.Bot.*;
-
-/** The type BotAction, an abstract superclass for all bot actions. */
+/**
+ * The type BotAction, an abstract superclass for all bot actions.
+ */
 public abstract class BotAction {
-  protected Bot bot;
+    protected Bot bot;
 
-  /**
-   * Instantiates a new BotAction.
-   *
-   * @param bot the specified bot
-   */
-  protected BotAction(Bot bot) {
-    setBot(bot);
-  }
+    /**
+     * Instantiates a new BotAction.
+     *
+     * @param bot the specified bot
+     */
+    protected BotAction(Bot bot) {
+        setBot(bot);
+    }
 
-  /** Listener method for commands. */
-  public abstract void start();
+    /**
+     * Listener method for commands.
+     */
+    public abstract void start();
 
-  /**
-   * Sets the bot to the one specified.
-   *
-   * @param bot the bot
-   */
-  public void setBot(Bot bot) {
-    this.bot = bot;
-  }
+    /**
+     * Sets the bot to the one specified.
+     *
+     * @param bot the bot
+     */
+    public void setBot(Bot bot) {
+        this.bot = bot;
+    }
 
-  /**
-   * Gets the bot.
-   *
-   * @return the bot
-   */
-  public Bot getBot() {
-    return bot;
-  }
+    /**
+     * Gets the bot.
+     *
+     * @return the bot
+     */
+    public Bot getBot() {
+        return bot;
+    }
 }
