3773c4f0172d50d02975db36142849f40a6814d0
Cal <31387289+callumadair@users.noreply.github.com>
2021-02-04 17:19:59 +0000

    Update Token.java

--- src/main/java/com/github/callumadair/bot/control/Token.java
+++ src/main/java/com/github/callumadair/bot/control/Token.java
@@ -1,7 +1,7 @@
 package com.github.callumadair.bot.control;
 
 public enum Token {
-    TOKEN("Mzk4OTI5NDY3NjU0Nzk5Mzcw.Wk_aBA.Gkr-WTTVUDwy4c2XAr73KqRTXnI");
+    TOKEN("");
     String token;
     Token(String token) {
         this.token = token;
